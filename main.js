61c6063d2f761a37e36270d957187878127660df
CWirbka <CWirbka@aol.de>
2022-12-04 10:08:39 +0100

    first

---
+++ main.js
@@ -0,0 +1,23 @@
+// Objects 
+
+var human = {
+    name: "Bo", 
+    height: "173",
+    age: 15,
+    country: "DE",
+    children: ["Lucas", "Leonie"]
+}
+
+
+
+// If statements
+
+var age = prompt("Trage dein Alter ein:");
+
+if (age >= 18){
+    console.log("Eintritt zugelassen");
+} else {
+
+// nicht alt genug
+    console.log("Nicht alt genug");
+}
